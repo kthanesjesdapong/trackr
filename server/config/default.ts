7d503b32f8d1d9531b5d8f782a5d3cb438d0856d
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2022-12-05 23:33:00 -0800

    deleted seedTopic, cleaned up seed.ts, added endpoint in config/default.ts

--- server/config/default.ts
+++ server/config/default.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 export default {
   port: 8080,
   gqlEndpoint: process.env.endpoint,
-  LOCAL_API_ENDPOINT: process.env.LOCAL_API_ENDPOINT,
+  LEETCODE_API_ENDPOINT: process.env.LEETCODE_API_ENDPOINT,
   TABLE_NAME: process.env.TABLE_NAME,
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
