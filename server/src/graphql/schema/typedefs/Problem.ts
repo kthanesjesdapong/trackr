9b45806ce10188bb2314dad24cc4c4edc666a41d
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2022-12-09 18:34:11 -0800

    Added Cursor Pagination through prismaConnection and relayPlugin

--- server/src/graphql/schema/typedefs/Problem.ts
+++ server/src/graphql/schema/typedefs/Problem.ts
@@ -2,15 +2,17 @@ import { builder } from "../builder";
 
 
 //Defining what data is exposed through our API
-export const Problem = builder.prismaObject("Problem", {
+export const Problem = builder.prismaNode("Problem", {
+  id: { resolve: (problem) => problem.id },
+  findUnique: (id) => ({ id: Number.parseInt(id, 10) }),
   fields: (t) => ({
-    id: t.exposeInt('id'),
     title: t.exposeString('title'),
     titleSlug: t.exposeString('titleSlug'),
     difficulty: t.exposeString('difficulty'),
     acRate: t.exposeFloat('acRate'),
     status: t.exposeString('status'),
-    topics: t.relation('topics', {
+    topics: t.relatedConnection('topics', {
+      cursor: 'id',
       args: { topicId: t.arg.intList() },
       query: (args, context) => ({
         orderBy: {
