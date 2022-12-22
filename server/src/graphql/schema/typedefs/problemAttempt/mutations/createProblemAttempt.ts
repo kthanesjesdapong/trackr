3791cdb2b1d7417968b12fdf0705a0c2facf4654
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2022-12-21 20:39:50 -0800

    working on createProblemAttempt

--- server/src/graphql/schema/typedefs/problemAttempt/mutations/createProblemAttempt.ts
+++ server/src/graphql/schema/typedefs/problemAttempt/mutations/createProblemAttempt.ts
@@ -1,19 +1,23 @@
 import { builder } from '../../../builder';
-
+import { Problem } from '../../problems/Problem';
+import { ProblemAttemptDetails } from '@prisma/client';
 
 builder.mutationField('createProblemAttempt', (t) => t.prismaField({
   type: 'ProblemAttempt',
   args: {
     problemId: t.arg.int({ required: true }),
-    attemptDetailId: t.arg.int({ required: true })
+    attemptDetailId: t.arg.int({ required: true }),
+    userId: t.arg.string({ required: true }),
+    status: t.arg.string({ required: true }),
   },
   resolve: async (query, root, args, context, info) => {
     const createdProblemAttempt = await context.prisma.problemAttempt.create({
       data: {
+        ...query,
         problemId: args.problemId,
         attemptDetailId: args.attemptDetailId
       },
-      ...query
+
     });
     return createdProblemAttempt;
   }
@@ -23,7 +27,38 @@ builder.mutationField('createProblemAttempt', (t) => t.prismaField({
 /*
 
 
+t.arg.Problem.id
 
 
+create a problemAttempt,
+connect it with a problem through a problem Id,
+
+create a ProblemAttemptDetail
+and then connect it it with the attemptDetailId
+
+
+
+problemId: {
+  // this is already within our db
+  connect: {
+    id: 1 // num of problemId you are connecting
+  }
+},
+attemptDetailId:{
+  create: {
+    userId: 'kavin1'// this is going to be the username of the user
+    status: args.status,
+
+  }
+}
+
+
+
+}
+
+create the attemptDetailId, and then connect the newly created attemptDetailid to problemAttempt
+
+
+create type for createProblemAttemptInput
 
 */
