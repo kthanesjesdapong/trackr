a5f20a8b12ecdf87b7ff1280106524cb2d3dd045
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2023-01-26 20:24:06 -0800

    Added side navbar comp

---
+++ client/src/components/SideNavBar/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const SideNavBar: FC = () => {
+    return (
+        <div>index</div>
+    );
+};
--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import { TopNavBar } from '@/src/components/TopNavBar/TopNavBar';
 import { DashboardStyles } from './Dashboard.styles';
+import { SideNavBar } from '@/src/components/SideNavBar';
 
 export const Dashboard: FC = () => {
 
     return (
         <DashboardStyles>
             <TopNavBar />
+            <SideNavBar />
         </DashboardStyles>
 
     );
